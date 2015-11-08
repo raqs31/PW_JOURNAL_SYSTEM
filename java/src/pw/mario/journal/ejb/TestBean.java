14da9412d55016fe65e793522c2f253f3c77a449
raqs31 <raqs31@gmail.com>
2015-11-08 14:03:26 +0100

    nauki ciąg dalszy

--- java/src/pw/mario/journal/ejb/TestBean.java
+++ java/src/pw/mario/journal/ejb/TestBean.java
@@ -1,32 +1,39 @@
 package pw.mario.journal.ejb;
 
+import java.io.Serializable;
 import java.util.LinkedList;
+
 import java.util.List;
 
-import javax.faces.bean.ApplicationScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
-import org.primefaces.push.annotation.Singleton;
+import org.primefaces.event.TabChangeEvent;
+import org.primefaces.event.TabCloseEvent;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-public class TestBean {
+@Data
+@ManagedBean(name="testBean")
+@SessionScoped
+public class TestBean implements Serializable {
 	public TestBean() {
 		testList = new LinkedList<>();
-		testList.add("Mario");
-		testList.add("Bro");
+		for (int i = 0; i < 20; i++) {
+			testList.add("test"+i);
+		}
 	}
-	
+	private String selected;
 	private List<String> testList;
-
-	public List<String> getTestList() {
-		return testList;
+	
+	public void onTabClose(TabCloseEvent e) {
+		FacesMessage msg = new FacesMessage("TabClose", e.getTab().getTitle());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
-
-	public void setTestList(List<String> testList) {
-		this.testList = testList;
+	public void onTabOpen(TabChangeEvent e) {
+		FacesMessage msg = new FacesMessage("TabChange", e.getTab().getTitle());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 }
