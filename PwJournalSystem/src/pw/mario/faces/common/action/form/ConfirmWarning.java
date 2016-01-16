3132017f4645b01db36b77d5c4657391681d49cf
raqs31 <raqs31@gmail.com>
2016-01-16 21:22:26 +0100

    poprawki przekierowań

--- PwJournalSystem/src/pw/mario/faces/common/action/form/ConfirmWarning.java
+++ PwJournalSystem/src/pw/mario/faces/common/action/form/ConfirmWarning.java
@@ -1,20 +1,13 @@
 package pw.mario.faces.common.action.form;
 
 
-import java.io.IOException;
-
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.extern.log4j.Log4j;
 import pw.mario.faces.common.action.Action;
 import pw.mario.faces.common.exception.PerformActionException;
 
-@Log4j
 @Builder
 public class ConfirmWarning implements OnConfirmAction {
 	@Setter private Action action;
@@ -56,16 +49,8 @@ public class ConfirmWarning implements OnConfirmAction {
 
 	@Override
 	public String afterAction() {
-		FacesContext ctx = FacesContext.getCurrentInstance();
-		try {
-			if (actionSucces)
-				ctx.getExternalContext().redirect("users.xhtml");
-		} catch (IOException e) {
-			log.error("Error while redirect", e);
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Błąd podczas przekierowania", null);
-			ctx.addMessage(null, msg);
-			e.printStackTrace();
-		}
+		if (actionSucces)
+			return getToRedirect();
 		return null;
 	}
 }
