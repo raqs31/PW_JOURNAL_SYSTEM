8f48c7a1eef77044597f0a12d7f725fb56c032c0
raqs31 <raqs31@gmail.com>
2016-02-14 14:14:17 +0100

    Obsługa wyjatku ViewExpiredException + filtr przekierowujaćy

---
+++ PwJournalSystem/src/pw/mario/faces/credential/filter/NoCacheFilter.java
@@ -0,0 +1,42 @@
+package pw.mario.faces.credential.filter;
+
+import java.io.IOException;
+
+import javax.faces.application.ResourceHandler;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(servletNames="Faces Servlet")
+public class NoCacheFilter implements Filter{
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        if (!req.getRequestURI().startsWith(req.getContextPath() + ResourceHandler.RESOURCE_IDENTIFIER)) {
+            res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+            res.setHeader("Pragma", "no-cache");
+            res.setDateHeader("Expires", 0);
+        }
+
+        chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
